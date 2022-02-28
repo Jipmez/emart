3930bae90e43707dd620ec5a502e8e5e519135f8
Jipmez <Mezj972@gmail.com>
2022-02-28 21:20:41 +0100

    emart1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
-export const environment = {
+/* export const environment = {
   production: false,
   serverURL: "http://localhost:3000/api/",
+}; */
+
+export const environment = {
+  production: false,
+  serverURL: "https://ecom.stcbnk.com/api/",
 };
